8eb5b8c478ff40d434742c53f7b4720a4f721fd9
Bruna Magalhães Criscuolo <bruna.magalhaes@levva.io>
2023-10-02 18:44:06 -0300

    feat: create movies list and show one movie name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart%20';
+import 'package:http/http.dart';
+import 'package:movies_v2/movies_list.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,26 +17,43 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  void _getListAPI() {
-    http.get(Uri.https('api.themoviedb.org', '/4/list/1'), headers: {
-      'authorization': 'Bearer xxxx',
-      'content-type': 'application/json;charset=utf-8'
-    }).then((Response value) => print(value.body));
+  MoviesList? movies;
+
+  Future<void> _getListagemAPI() async {
+    await Future.delayed(Duration(seconds: 5));
+    http.get(
+      Uri.https('api.themoviedb.org', '/4/list/1'),
+      headers: {
+        'authorization':
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDhmYWNkMDFkMDI5ZTI4ZTlmNDNmNTQyNmMxZWIyZSIsInN1YiI6IjYwZTQ3OTgyNmVlM2Q3MDAyYzI4ODUwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcmY0hq1lGeqG_TG_0SsBZGuSBW8jfhkApJbydn2t0U',
+        'content-type': 'application/json;charset=utf-8'
+      },
+    ).then(
+      (Response value) {
+        if (value.statusCode == 200) {
+          movies = MoviesList.fromJson(
+            jsonDecode(value.body),
+          );
+          print(movies.toString());
+          setState(() {});
+        }
+      },
+    );
   }
 
   @override
   void initState() {
     super.initState();
-    _getListAPI();
+    _getListagemAPI();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: Center(
-          child: Text('Vamos ver se funciona!'),
+          child: Text('Movie: ${movies?.name ?? "Erro"}'),
         ),
       ),
     );
