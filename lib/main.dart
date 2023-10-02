e1989512684e6ad2d0251bd2e8df6b8c4420fe70
Bruna Magalhães Criscuolo <bruna.magalhaes@levva.io>
2023-10-02 12:25:08 -0300

    feat: create get list implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart%20';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    http
-        .get(Uri.https('xptoflutter.free.beeceptor.com', '/test'))
-        .then((value) => print(value.body));
+  State<MainApp> createState() => _MainAppState();
+}
 
+class _MainAppState extends State<MainApp> {
+  void _getListAPI() {
+    http.get(Uri.https('api.themoviedb.org', '/4/list/1'), headers: {
+      'authorization':
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDhmYWNkMDFkMDI5ZTI4ZTlmNDNmNTQyNmMxZWIyZSIsInN1YiI6IjYwZTQ3OTgyNmVlM2Q3MDAyYzI4ODUwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NcmY0hq1lGeqG_TG_0SsBZGuSBW8jfhkApJbydn2t0U',
+      'content-type': 'application/json;charset=utf-8'
+    }).then((Response value) => print(value.body));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getListAPI();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
